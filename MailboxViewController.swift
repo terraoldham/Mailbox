e5d839dd6d19db4dd78cbda09e166846f022ffca
Terra Oldham <toldham@hearsaycorp.com>
2014-10-21 20:04:53 -0700

    Initial Commit
    Launch Screen and Scroll View

---
+++ MailboxViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MailboxViewController.swift
+//  Mailbox
+//
+//  Created by Terra Oldham on 10/21/14.
+//  Copyright (c) 2014 HearsaySocial. All rights reserved.
+//
+
+import UIKit
+
+class MailboxViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: 320, height: 2175)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
