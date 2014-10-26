495473bd4915fb50374d21e6496ec33a9b649b88
Terra Oldham <toldham@hearsaycorp.com>
2014-10-26 16:47:55 -0700

    Added Pan Gesture Recognizer
    Added Pan Gesture Recognizer and connected View with messages within
    in.

--- MailboxViewController.swift
+++ MailboxViewController.swift
@@ -10,28 +10,41 @@ import UIKit
 
 class MailboxViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var messageView: UIImageView!
+    @IBOutlet weak var laterView: UIImageView!
+    @IBOutlet weak var listView: UIImageView!
+    @IBOutlet weak var deleteView: UIImageView!
+    @IBOutlet weak var archiveView: UIImageView!
+    @IBOutlet var panGestureRecognizer: UIPanGestureRecognizer!
+    @IBOutlet weak var combinedView: UIView!
+    
+    
+    @IBAction func onDrag(sender: UIPanGestureRecognizer){
+        
+        var point = panGestureRecognizer.locationInView(view)
+        var velocity = panGestureRecognizer.velocityInView(view)
+        var translation = panGestureRecognizer.translationInView(view)
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began{
+            
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            
+        }
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.contentSize = CGSize(width: 320, height: 2325)
+        combinedView.addGestureRecognizer(panGestureRecognizer)
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
