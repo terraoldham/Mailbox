6b9f599bff82de4ab065b595d71f4215db53728d
Terra Oldham <toldham@hearsaycorp.com>
2014-10-26 19:06:51 -0700

    Colors
    Adding Colors

--- MailboxViewController.swift
+++ MailboxViewController.swift
@@ -17,6 +17,7 @@ class MailboxViewController: UIViewController {
     @IBOutlet weak var archiveView: UIImageView!
     @IBOutlet var panGestureRecognizer: UIPanGestureRecognizer!
     @IBOutlet weak var combinedView: UIView!
+    var messageCenter: CGPoint!
     
     
     @IBAction func onDrag(sender: UIPanGestureRecognizer){
@@ -24,10 +25,33 @@ class MailboxViewController: UIViewController {
         var point = panGestureRecognizer.locationInView(view)
         var velocity = panGestureRecognizer.velocityInView(view)
         var translation = panGestureRecognizer.translationInView(view)
+        messageView.userInteractionEnabled = true 
         
         if panGestureRecognizer.state == UIGestureRecognizerState.Began{
+            messageCenter = messageView.center
             
         } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+                self.messageView.center.x = point.x
+            
+                println("Gesture changed at: \(point)")
+                if messageCenter.x <= 160 && messageCenter.x > 50 {
+                    combinedView.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1)
+                    
+                } else if messageCenter.x < 50 {
+                    combinedView.backgroundColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1)
+                    
+                //swiping far left
+                } else if messageCenter.x > 160 && messageCenter.x < 280 {
+                    combinedView.backgroundColor = UIColor(red: 0.6, green: 0.4, blue: 0.2, alpha: 1)
+                
+                //swiping right
+                } else if messageCenter.x > 280 {
+                    
+                    
+                    combinedView.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 1)
+                
+                }
+            
             
         } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
             
